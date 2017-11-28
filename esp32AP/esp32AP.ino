2f0bba58eb1eed680d8fa6df1aef1fe600a95afe
Alex <atags22@gmail.com>
2017-11-28 16:04:31 -0500

    esp32AP now has meaningful content

--- esp32AP/esp32AP.ino
+++ esp32AP/esp32AP.ino
@@ -1,6 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+//Sample code from
+//https://techtutorialsx.com/2017/07/26/esp32-arduino-setting-a-soft-ap/
 
+#include <WiFi.h>
+
+const char* ssid = "stringHere";
+const char* pass = "passHere";
+
+
+void setup() {
+  //start the serial coms
+  Serial.begin(115200);
+
+  //start the AP
+  WiFi.softAP(ssid,pass);
+
+  Serial.print("IP: ");
+  Serial.println(WiFi.softAPIP());
+  
 }
 
 void loop() {
